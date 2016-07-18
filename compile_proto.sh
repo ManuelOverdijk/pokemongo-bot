5055e51205f9c567d33b6ae59711dff94244bfe8
Khaled Nassar <kmhnassar@gmail.com>
2016-07-18 13:37:26 +0200

    Fixing python import system, .gitignore compiled bytecode

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,2 +1,15 @@
-cd "POGOProtos"
-./compile.py -l python -o ../protocol
+PROTOS_LIB="POGOProtos"
+PROTOCOL_OUT="protocol"
+INIT="__init__.py"
+
+cd "$PROTOS_LIB"
+./compile.py -l python -o ../"$PROTOCOL_OUT"
+
+cd ..
+
+for d in "$PROTOCOL_OUT"/*/; do
+	echo "" > "${d}__init__.py"
+	echo "Setting up module $d"
+done
+
+printf "import os\nimport sys\nsys.path.append(os.path.dirname(__file__))" > "$PROTOCOL_OUT/__init__.py"
